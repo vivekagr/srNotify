781877530fce41807e4d89d36c426392f4964575
vivekagr <me@vivek.im>
2013-01-13 20:55:12 +0530

    my first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+
+# This script looks up /r/gamedeals/new every 120 seconds and pushes the notification
+# for new posts to Growl app on Mac OS
+
+# Uses PRAW - https://github.com/praw-dev/praw (for easy access to Reddit API)
+# and GNTP - https://github.com/kfdm/gntp (for pushing Growl notification)
+
+import gntp.notifier
+import praw
+import time
+import sys
+
+# icon for use with growl notification
+ICON_URL = "http://cdn2.iconfinder.com/data/icons/crystalproject/128x128/apps/package_games.png"
+USER_AGENT = 'new /r/gamedeals notifier by & for /u/mpheus'
+
+growl = gntp.notifier.GrowlNotifier(
+    applicationName = "/r/gamedeals notifier",
+    notifications = ["New Deal"],
+    defaultNotifications = ["New Deal"],
+    # hostname = "computer.example.com", # Defaults to localhost
+    # password = "abc123" # Defaults to a blank password
+)
+growl.register()
+
+r = praw.Reddit(user_agent=USER_AGENT)
+
+already_done = [] # for storing the uids of posts already notified
+first_time = True
+
+while True:
+	try:
+		data = r.get_subreddit('gamedeals').get_new_by_date(limit=10)
+		for x in data:
+			if x.id not in already_done:
+				 # so that notification remains on the screen until closed
+				stick = False if first_time else True
+				growl.notify(
+					noteType = "New Deal",
+					title = x.domain,
+					description = x.title,
+					icon = ICON_URL,
+					sticky = stick,
+					priority = 1,
+					callback = x.permalink
+				)
+				already_done.append(x.id)
+				print "Notified about", x.title, "at", time.strftime("%d %b - %I:%M:%S %p")
+				
+		print "Last checked for game deals at", time.strftime("%d %b - %I:%M:%S %p")
+		time.sleep(120)
+		first_time = False
+		
+	except Exception, e:
+		print "Error " + str(e) + " caught at " + time.strftime("%d %b - %I:%M:%S %p")
+		time.sleep(120)
